d051ca480c3ceb37f9ef0d87883e8f0e078adbff
CryptoKstar <toppythondev13579@gmail.com>
2021-12-08 01:54:41 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -111,7 +111,7 @@ const Home = () => {
         );
         const walletB = await spinT.methods.balanceOf(account).call();
         const totalMint = await spinT.methods.totalSupply().call();
-        const totalstaked = await spinC.methods.totalSupply().call();
+        const totalstaked = await spinC.methods.totalStaked().call();
         const totalburned = await spinT.methods.totalSupply().call();
         const harvestedValue = await spinF.methods.pendingSpintop(account).call();
         setWalletBalance(fromWei(web3, walletB).toString())
