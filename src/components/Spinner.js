696f02aa755c5f423e09e765de98b414d902a721
CryptoKstar <toppythondev13579@gmail.com>
2021-12-08 15:34:02 +0900

    update

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -3,7 +3,7 @@ import React from "react";
 const Spinner = () => {
     return (
         <div>
-            <div className="loading">
+            <div className="loading-div">
                 <div className="loader">
                     <span className="loading-span"></span>
                     <span className="loading-span"></span>
