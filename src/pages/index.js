a34862b0b74b5839dbed6f2ea07f845b857fd639
CryptoKstar <toppythondev13579@gmail.com>
2021-12-13 00:26:31 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -257,8 +257,7 @@ const Home = () => {
                   </Box>
                   <img src="./assets/images/twitter-2.svg" alt="" className="twit" />
                 </Box>
-                <iframe className="twitter-timeline" data-height="380" data-theme="dark" src="https://twitter.com/SpintopNetwork?ref_src=twsrc%5Etfw">Tweets by SpintopNetwork</iframe>
-		            {/* <a >Tweets by SpintopNetwork</a> */}
+		            <a className="twitter-timeline" data-height="380" data-theme="dark" href="https://twitter.com/SpintopNetwork?ref_src=twsrc%5Etfw">Tweets by SpintopNetwork</a>
               </Box>
             </Col>
           </Row>
